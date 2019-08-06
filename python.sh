b285a49c84dfa6dcb4d832bfd22a65223fd619af
ACworks-AI-Lab <ai-lab@acworks.co.jp>
2019-08-06 07:14:10 +0000

    Fix python script

--- python.sh
+++ python.sh
@@ -14,7 +14,6 @@ sudo apt-get install -y \
 
 pip3 --version
 pip3 install -U virtualenv
-which virtualenv
 pip3 install -U virtualenvwrapper
 pip3 install -U jupyter jupyterlab jupyter_http_over_ws
 
@@ -26,10 +25,10 @@ echo "source ~/.local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 
 # Install TensorFlow on Jetson Platform
 # Reference: https://docs.nvidia.com/deeplearning/frameworks/install-tf-jetson-platform/index.html
-sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev
-sudo pip3 install -U pip
-sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta
-3
-sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
+#sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev
+#sudo pip3 install -U pip
+#sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta
+#3
+#sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
 
 echo -e "\\033[0;32m[OK]\\033[0m"
